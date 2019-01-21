1d1cd5e5e8f2a220546ff0a005dd3aed27cac561
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 20:31:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,9 @@ class PostsController < ApplicationController
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	def post_params
+	  params.require(:post).permit(:title, :description)
+	end
+	
 end
